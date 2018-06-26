62f5cdcd7490a12dd1613afbb8bd7f689228cb27
Fan Hongtao <fanhongtao@gmail.com>
2018-06-26 20:18:24 +0800

    递归处理 Blog 中所有 HTML 文件 （同时记录下URL） 为了避免因为 http:// 和 https:// 不同，而导致无意义的重复，所以将 start_urls 修改成 http://fanhongtao.github.io/

--- myblog/myblog/spiders/blog.py
+++ myblog/myblog/spiders/blog.py
@@ -1,18 +1,37 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import os
 
 class BlogSpider(scrapy.Spider):
     name = 'blog'
     allowed_domains = ['fanhongtao.github.io']
-    start_urls = ['http://fanhongtao.github.io/']
+    start_urls = ['https://fanhongtao.github.io/']
+
+    def __init__(self):
+        super().__init__();
+        self.url_file = "visited_url.txt"
+        self.html_path = "saved"
+        if (not os.path.exists(self.html_path)):
+            os.mkdir(self.html_path)
+        if (os.path.exists(self.url_file)):
+            os.remove(self.url_file)
 
     def parse(self, response):
+        self.log_url(response)
         self.save_html(response)
+        for href in response.css('a::attr(href)'):
+            url = href.extract()
+            if (url.endswith(".html") or url.endswith(".htm") or url.endswith("/")):
+                yield response.follow(href, callback=self.parse)
+
+    def log_url(self, response):
+        with open(self.url_file, 'a') as f:
+            f.write(response.url)
+            f.write("\n")
 
     def save_html(self, response):
         page = response.url.split("//")[-1]
-        filename = page.strip("/").replace("/", "_")
+        filename = self.html_path + "/" + page.strip("/").replace("/", "_")
         if (not (filename.endswith(".html") or filename.endswith(".htm"))):
             filename = filename + ".html"
         with open(filename, 'wb') as f:
