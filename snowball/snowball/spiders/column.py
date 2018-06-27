8e9c9a7341e2da869fc689cbbd7fa2a361d0138c
Fan Hongtao <fanhongtao@gmail.com>
2018-06-28 07:25:20 +0800

    添加记录返回html的方法 （此时返回的 html 中并没有我想要获取的数据）

--- snowball/snowball/spiders/column.py
+++ snowball/snowball/spiders/column.py
@@ -10,4 +10,13 @@ class ColumnSpider(scrapy.Spider):
         yield scrapy.Request('http://xueqiu.com/%s/column' % self.userid)
 
     def parse(self, response):
-        pass
+        self.save_html(response)
+
+    def save_html(self, response):
+        page = response.url.split("//")[-1]
+        filename = page.strip("/").replace("/", "_")
+        if (not (filename.endswith(".html") or filename.endswith(".htm"))):
+            filename = filename + ".html"
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
