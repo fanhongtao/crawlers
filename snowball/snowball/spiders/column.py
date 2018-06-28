acd843d87342910659db2da489d60f74e551dbe0
Fan Hongtao <fanhongtao@gmail.com>
2018-06-28 09:33:45 +0800

    添加调用 selenium 的代码

--- snowball/snowball/spiders/column.py
+++ snowball/snowball/spiders/column.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from selenium import webdriver
 
 class ColumnSpider(scrapy.Spider):
     name = 'column'
     allowed_domains = ['xueqiu.com']
 
-    def start_requests(self):
-        yield scrapy.Request('http://xueqiu.com/%s/column' % self.userid)
+    def __init__(self, userid = None):
+        super().__init__()   # 或 scrapy.Spider.__init__(self)
+        self.start_urls = ['http://xueqiu.com/%s/column' % userid]
+        self.driver = webdriver.Firefox()
+        self.driver.set_page_load_timeout(10)
 
     def parse(self, response):
         self.save_html(response)
@@ -20,3 +24,13 @@ class ColumnSpider(scrapy.Spider):
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+    def closed(self, reason):
+        # Spider中没有 closed 方法，所以不需要如下语句
+        # super().closed(reason);
+        try:
+            self.driver.close();
+        except Exception as e:
+            self.log(e)
+        self.driver.quit()
+        self.log("webdriver closed.")
