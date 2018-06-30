f8a882c8de9b68cbc884b8c77746666b016ec86c
Fan Hongtao <fanhongtao@gmail.com>
2018-06-30 08:12:31 +0800

    修改代码结构

--- snowball/column_viewer.py
+++ snowball/column_viewer.py
@@ -3,46 +3,49 @@
 import json
 import os
 import webbrowser
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk  #导入内部包
 
-def treeviewClick(event):
-    item = tree.selection()[0]
-    item_text = tree.item(item, "values")
-    # print(item_text[0])
-    if (len(item_text) > 1):
-        filename = "file://" + os.path.abspath(item_text[1])
-        print("Open file: " + filename)
-        webbrowser.open(filename)
+class App(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.tree = ttk.Treeview(master)
+        self.load_items()
+        self.tree.bind('<ButtonRelease-1>', self.treeview_click)
+        self.tree.pack(expand=tk.YES, fill=tk.BOTH)
 
-win = Tk()
-#w = win.winfo_screenwidth()
-#h = win.winfo_screenheight()
-#win.geometry("%dx%d" %(w, h))
+    def load_items(self):
+        user_count = 0
+        for userid in os.listdir("column"):
+            user = self.tree.insert("", user_count, text=userid, values=(userid))
+            column_path = "column/" + userid
+            filename = column_path + "/" + "column_" + userid + ".json"
+            with open(filename, 'r', encoding='utf-8') as f:
+                items = json.load(f)['items']
+
+            item_count = 0
+            for item in items:
+                title = item['title']
+                url = item['link']
+                page = url.split("//")[-1]
+                htmlfile = column_path + "/" + page.strip("/").replace("/", "_")
+                if (not (htmlfile.endswith(".html") or htmlfile.endswith(".htm"))):
+                    htmlfile = htmlfile + ".html"
+                self.tree.insert(user, item_count, text=title, values=(url, htmlfile))
+                item_count = item_count + 1
+            user_count = user_count + 1
+
+    def treeview_click(self, event):
+        item = self.tree.selection()[0]
+        item_text = self.tree.item(item, "values")
+        if (len(item_text) > 1):
+            filename = "file://" + os.path.abspath(item_text[1])
+            print("Open file: " + filename)
+            webbrowser.open(filename)
+
+
+win = tk.Tk()
 w, h = win.maxsize()
-win.geometry("{}x{}".format(w, h)) #看好了，中间的是小写字母x
-tree = ttk.Treeview(win)
-
-user_count = 0
-for userid in os.listdir("column"):
-    user = tree.insert("", user_count, text=userid, values=(userid))
-    column_path = "column/" + userid
-    filename = column_path + "/" + "column_" + userid + ".json"
-    with open(filename, 'r', encoding='utf-8') as f:
-        items = json.load(f)['items']
-
-    item_count = 0
-    for item in items:
-        title = item['title']
-        url = item['link']
-        page = url.split("//")[-1]
-        htmlfile = column_path + "/" + page.strip("/").replace("/", "_")
-        if (not (htmlfile.endswith(".html") or htmlfile.endswith(".htm"))):
-            htmlfile = htmlfile + ".html"
-        tree.insert(user, item_count, text=title, values=(url, htmlfile))
-        item_count = item_count + 1
-    user_count = user_count + 1
-
-tree.bind('<ButtonRelease-1>', treeviewClick)
-tree.pack(expand=YES, fill=BOTH)
-win.mainloop()
+win.geometry("{}x{}".format(w, h))
+app = App(win)
+app.mainloop()
